1000b8000c9521133cd04b906c773822aedbe84c
jian-level-is <myunggeun22@gmail.com>
2021-08-05 21:22:33 +0900

    Create Main.java

---
+++ algorithms100/src/_09/Main.java
@@ -0,0 +1,19 @@
+package _09;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int num = 1242;
+		
+		int sum = 0;
+		
+		while(num > 0) {
+			sum += num % 10;
+			num /= 10;
+		}
+		System.out.println(sum);
+		
+	
+	}
+
+}

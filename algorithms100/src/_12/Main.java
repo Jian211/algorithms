cd5d3c1e67796f2af58f4077eba62a146f706ed1
jian-level-is <myunggeun22@gmail.com>
2021-08-08 20:06:03 +0900

    Create Main.java

---
+++ algorithms100/src/_12/Main.java
@@ -0,0 +1,32 @@
+package _12;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int n = 5;
+		
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				System.out.printf("%3d", j * n + 1 + i);
+			}
+			System.out.println();
+		}
+		
+		System.out.println("영상 본 후,,,");
+		
+		int b = 1;
+		int [][]arr = new int [n][n];
+		for(int i=0 ; i < n ; i++) {
+			for (int j = 0; j < n; j++) {
+				arr[j][i] = b++;
+			}
+		}
+		
+		for(int i=0 ; i < n ; i++) {
+			for (int j = 0; j < n; j++) {
+				System.out.printf("%3d",arr[i][j]);
+			}
+			System.out.println();
+		}
+	}
+}

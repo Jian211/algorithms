fad05a3a8a135629a55459e3044a3280b9a026eb
jian-level-is <myunggeun22@gmail.com>
2021-08-16 17:58:49 +0900

    Create Involution.java

---
+++ algorithms100/src/_18/Involution.java
@@ -0,0 +1,17 @@
+package _18;
+
+public class Involution {
+
+	public static void main(String[] args) {
+		int a,b;
+		a = 5;
+		b = 3;
+		
+		int result = 1;
+		
+		for(int i=0 ; i < b ; i++) {
+			result = result * a;
+		}
+		System.out.println(result);
+	}
+}

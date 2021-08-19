48e18f59aa9665bc227e462e7de2e16fccf82ed3
jian-level-is <myunggeun22@gmail.com>
2021-08-19 16:52:16 +0900

    Create Reverse.java

---
+++ algorithms100/src/_22/Reverse.java
@@ -0,0 +1,33 @@
+package _22;
+
+public class Reverse {
+
+	static String way1 (String word) {
+		char [] arr = new char[word.length()];
+		String result = "";
+		
+		for(int i = word.length()-1 ; i >= 0 ;i--) {
+			arr[i] = word.charAt(i);
+			result += word.charAt(i);
+		}
+		return result;
+	}
+	
+	static String way2 (String word) {
+		char[] arr = word.toCharArray();
+		char tmp;
+
+		for(int i=0 ; i < arr.length/2 ; i++) {
+			tmp = arr[i];
+			arr[i] = arr[arr.length-i-1];
+			arr[arr.length-i-1] = tmp;
+		}
+		return new String(arr);
+	}
+	
+	public static void main(String[] args) {
+		String word = "ADEFH";
+		System.out.println(way1(word));
+		System.out.println(way2(word));
+	}
+}

0830f890f3f4ec7a64c356fcc9795690e2e98fea
jian-level-is <myunggeun22@gmail.com>
2021-09-02 22:14:23 +0900

    Create Fatorial.java

---
+++ algorithms100/src/_30/Fatorial.java
@@ -0,0 +1,18 @@
+package _30;
+
+public class Fatorial {
+
+	static int fatorial(int num) {
+		if(num == 1) return 1;
+		return num * fatorial(num-1);
+	}
+	
+	public static void main(String[] args) {
+		int num = 5;
+		int result = 0;
+		
+		result = 0;
+		
+		System.out.println(fatorial(4));
+	}
+}

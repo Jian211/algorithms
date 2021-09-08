a117f6f6fae02fcccc349cb0a484c325ca6447bf
jian-level-is <myunggeun22@gmail.com>
2021-09-08 22:15:43 +0900

    Update Binary.java

--- algorithms100/src/algorithm/Binary.java
+++ algorithms100/src/algorithm/Binary.java
@@ -1,38 +1,24 @@
 package algorithm;
 
-import java.util.Iterator;
-
 public class Binary {
-	static int result ;
-	static String chg = "";
-	static int x = 0;	
-	
-	static int changeA (String a ) {
-		int ok = 0;
-		for (int i = 0; i < a.length(); i++) {
-			
-		}
-		
-		return a;
-	}
-	
-	static int binary(int n) {
-		System.out.println("현재의 n의 값은 > "+ n);
+
+	static void binary(int n) {
 		if(n == 1 || n == 0) {
-			chg += Integer.toString(n);
-			System.out.println("result의 값은 > "+chg);
-			return n;
+			System.out.print(n);
+			return ;
 		}
-		System.out.println(n%2);
-		chg += Integer.toString(n%2);
-		System.out.println("현재의 result의 값은 > "+ result);
-		x++;
-		System.out.println("---------------");
-		return binary(n/2);
+		binary(n/2);
+		System.out.print(n%2);
 	}
 	
 	public static void main(String[] args) {
-		int num = 17;
+		// 17
+		// 8..1
+		// 4..0
+		// 2..0
+		// 1..0
+		
+		int num = 10;
 		
 		binary(num);
 		

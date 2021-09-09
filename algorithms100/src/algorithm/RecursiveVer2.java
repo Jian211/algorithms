b0583419005752493e2511ce8e4b6f1a9951ce67
jian-level-is <myunggeun22@gmail.com>
2021-09-09 22:24:18 +0900

    Create RecursiveVer2.java

---
+++ algorithms100/src/algorithm/RecursiveVer2.java
@@ -0,0 +1,19 @@
+package algorithm;
+
+public class RecursiveVer2 {
+
+	public static void main(String[] args) {
+		// 단지수 측정 재귀ver
+		int [][] homeTown = {{0,0,0,1,1},
+							 {0,0,0,0,1},
+							 {1,0,0,0,0},
+							 {1,1,0,0,0},
+							 {1,1,0,1,1},
+		};
+		// 붙어있으면 크게 하나의 단지
+		// 하나의 단지 속 1의 갯수가 세대수
+		// 단지수와 세대수를 구하시오
+		
+			
+	}
+}

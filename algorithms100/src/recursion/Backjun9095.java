e5fbe83f7274fa4532fd7186d2a3bfb1c95d8f36
jian-level-is <myunggeun22@gmail.com>
2021-09-23 15:49:04 +0900

    Update Backjun9095.java

--- algorithms100/src/recursion/Backjun9095.java
+++ algorithms100/src/recursion/Backjun9095.java
@@ -1,8 +1,8 @@
 package recursion;
 /*
  	정수 4를 1,2,3의 합으로 나타내는 방법은 총 7가지가 있다. 합을 나타낼 때는 수를 1개 이상 사용해야한다.
-	3+1
-	2+2
+	3+1    4 = 3+1	>	4 = (2+1)+1	 >	4 = ((1+1)+1)+1
+ 	2+2
 	2+1+1
 	1+3
 	1+2+1
@@ -21,7 +21,6 @@ package recursion;
 public class Backjun9095 {
 
 	public static void main(String[] args) {
-
 	}
 
 }
